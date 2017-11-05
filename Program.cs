77f2640805051fc648fc163a156f258a5d5a8dc7
KIvanovvv <32482919+KIvanovvv@users.noreply.github.com>
2017-11-05 23:52:24 +0200

    Add files via upload
    The Bigest integer

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBigestInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type 3 integers : " );
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+            Console.WriteLine("The biggest integers is : ");
+            
+
+            
+            Console.WriteLine(Math.Max(Math.Max(x, y), z));
+        }
+
+      
+    }
+}
+
